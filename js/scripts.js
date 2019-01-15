08225335db43c6d8310d1e0c0a2799b74457cfc4
Chris Younger, Denny Le and maddy haldeman <denny@>
2019-01-15 15:23:09 -0800

    we accidentally the whole thing.

---
+++ js/scripts.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+    $("#title1").click(function() {
+      $("p#1").toggleClass("def");
+    });
+    $("#title2").click(function() {
+        $("p#2").toggleClass("def");
+    });
+    $("#title3").click(function() {
+        $("p#3").toggleClass("def");
+    });
+    $("#title4").click(function() {
+        $("p#4").toggleClass("def");
+    });
+    $("#title5").click(function() {
+        $("p#5").toggleClass("def");
+    });
+    $("#title6").click(function() {
+        $("p#6").toggleClass("def");
+    });
+    $("#title7").click(function() {
+        $("p#7").toggleClass("def");
+    });
+    $("#title8").click(function() {
+        $("p#8").toggleClass("def");
+    });
+    $("#title9").click(function() {
+        $("p#9").toggleClass("def");
+    });
+    $("#title10").click(function() {
+        $("p#10").toggleClass("def");
+    });
+    $("#title11").click(function() {
+        $("p#11").toggleClass("def");
+    });
+    $("#title12").click(function() {
+        $("p#12").toggleClass("def");
+    });
+    $("#title13").click(function() {
+        $("p#13").toggleClass("def");
+    });
+    $("#title14").click(function() {
+        $("p#14").toggleClass("def");
+    });
+    $("#title15").click(function() {
+        $("p#15").toggleClass("def");
+    });
+    $("#title16").click(function() {
+        $("p#16").toggleClass("def");
+    });
+    $("#title17").click(function() {
+        $("p#17").toggleClass("def");
+    });
+    $("#title18").click(function() {
+        $("p#18").toggleClass("def");
+    });
+    $("#title19").click(function() {
+        $("p#19").toggleClass("def");
+    });
+});
